320f3a679a17032e41d86e96bd0627f303146703
Marco Pizarro <marcoinnovates@gmail.com>
2022-02-27 21:38:11 -0800

    new thresh

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
 #include <Arduino.h>
 #include <Metro.h>
 
-int sensorL = 1;
-int sensorR = 2;
-int sensorM = 3;
+int sensorL = 21;
+int sensorR = 20;
+int sensorM = 19;
 
 int enablePinA = 16;
 int dirPin1A = 17;
@@ -13,19 +13,24 @@ int enablePinB = 9;
 int dirPin1B = 8;
 int dirPin2B = 7;
 
-int BlackH = 200;
-int BlueH = 400;
-int RedH = 800;
+int BlackL = 200;
+int BlackM = 70;
+int BlackR = 200;
+
+int speedL = 150;
+int speedR = 150;
 
 const int black = 0;
 const int blue = 1;
 const int red = 2;
 const int white = 3;
 
-int color(int sensorRead){
-  if(sensorRead <= BlackH){return black;}
-  else if(sensorRead <= BlueH){return blue;}
-  else if(sensorRead <= RedH){return red;}
+byte incoming;
+
+int color(int sensorRead, int blackT){
+  if(sensorRead <= blackT){return black;}
+  // else if(sensorRead <= BlueH){return blue;}
+  // else if(sensorRead <= RedH){return red;}
   else {return white;}
 }
 
@@ -44,35 +49,60 @@ void setup(){
 }
 
 void loop(){
-  analogWrite(enablePinA, 255);
-  analogWrite(enablePinB, 255);
+  // analogWrite(enablePinA, speedL);
+  // analogWrite(enablePinB, speedR);
 
-  digitalWrite(dirPin1A, HIGH);
+  // digitalWrite(dirPin1A, HIGH);
+  // digitalWrite(dirPin2A, LOW);
+
+  // digitalWrite(dirPin1B, HIGH);
+  // digitalWrite(dirPin2B, LOW);
+
+  int sensorLRead = color(analogRead(sensorL), BlackL);
+  int sensorRRead = color(analogRead(sensorR), BlackR);
+  int sensorMRead = color(analogRead(sensorM), BlackM);
+
+
+ if (Serial.available() > 0)
+  {
+  incoming = Serial.read();
+  digitalWrite(dirPin1A, LOW);
   digitalWrite(dirPin2A, LOW);
 
-  digitalWrite(dirPin1B, HIGH);
+  digitalWrite(dirPin1B, LOW);
   digitalWrite(dirPin2B, LOW);
-
-  // delay(2000);
-
-  // digitalWrite(dirPin1A, LOW);
-  // digitalWrite(dirPin2A, HIGH);
-
-  // digitalWrite(dirPin1B, LOW);
-  // digitalWrite(dirPin2B, HIGH);
-  // delay(2000);
-
-  int sensorLRead = color(analogRead(sensorL));
-  int sensorRRead = color(analogRead(sensorR));
-  int sensorMRead = color(analogRead(sensorM));
-
-  if(sensorLRead == white && sensorRRead == white && sensorMRead == black){
-    Serial.println("all good");
-  } else if(sensorMRead == black && sensorLRead != white && sensorRRead != white){
-    Serial.println("reached 90deg turn");
-  } else if(sensorMRead == black && sensorLRead != white && sensorRRead == white){
-    Serial.println("steer left"); 
-  } else if(sensorMRead == black && sensorLRead == white && sensorRRead != white){
-    Serial.println("steer right"); 
   }
+
+  Serial.print(sensorLRead);
+  Serial.print(sensorMRead);
+  Serial.println(sensorRRead);
+
+  // Serial.println(analogRead(sensorR));
+  // Serial.print(analogRead(sensorM));
+  // Serial.println(analogRead(sensorR));
+  delay(500);
+
+  // if(sensorLRead == white && sensorRRead == white && sensorMRead == black){
+  //   Serial.println("all good");
+  // } else if(sensorLRead != white && sensorRRead != white){
+  //   Serial.println("reached 90deg turn");
+  // } else if(sensorLRead != white && sensorRRead == white){
+  //   Serial.println("steer left"); 
+  //   while(sensorLRead != white && sensorRRead == white){
+  //     digitalWrite(dirPin1A, LOW);
+  //     digitalWrite(dirPin2A, HIGH);
+  //   }
+  //   digitalWrite(dirPin1A, HIGH);
+  //   digitalWrite(dirPin2A, LOW);
+  // } else if(sensorLRead == white && sensorRRead != white){
+  //   Serial.println("steer right"); 
+  //   while(sensorLRead == white && sensorRRead != white){
+  //     digitalWrite(dirPin1B, LOW);
+  //     digitalWrite(dirPin2B, HIGH);
+  //   }
+  //   digitalWrite(dirPin1B, HIGH);
+  //   digitalWrite(dirPin2B, LOW);
+  // } else{
+  //   Serial.println("UNKNOWN STATE"); 
+  // }
 }
